0b8cebb4b5137e6df6cd0c7852c17bc17c117027
laylap <pournajaf@gmail.com>
2015-04-13 15:33:59 -0400

    modified some files!

--- data-preprocessing/brinkhoff/discretize_brinkhoff_modified_postgresql.sql
+++ data-preprocessing/brinkhoff/discretize_brinkhoff_modified_postgresql.sql
@@ -1,10 +1,10 @@
-﻿--Descritize Locations g=32
-drop table if exists brinkhoff.b25k_modified_g_32;
+﻿--Descritize Locations g=1024
+drop table if exists brinkhoff.b25k_modified_g_1024;
 with dimensions as
 (
-select min(y) as minY, ((max(y)+2.0)-min(y))/32 as ySize, min(x) as minX, ((max(x)+2.0)-min(x))/32 as xSize
+select min(y) as minY, ((max(y)+2.0)-min(y))/1024 as ySize, min(x) as minX, ((max(x)+2.0)-min(x))/1024 as xSize
 from brinkhoff.b25k_modified_raw
 )
-SELECT id,x,y,vx,vy,t,trunc((x-minX)/xSize)*32+trunc((y-minY)/ySize) as r,trunc((x-minX)/xSize) as rx,trunc((y-minY)/ySize) as ry, (trunc((x-minX)/xSize) * xSize + xSize/2) as cx,(trunc((y-minY)/ySize) * ySize + ySize/2) as cy
-into brinkhoff.b25k_modified_g_32
+SELECT id,x,y,vx,vy,t,trunc((x-minX)/xSize)*1024+trunc((y-minY)/ySize) as r,trunc((x-minX)/xSize) as rx,trunc((y-minY)/ySize) as ry, (trunc((x-minX)/xSize) * xSize + xSize/2) as cx,(trunc((y-minY)/ySize) * ySize + ySize/2) as cy
+into brinkhoff.b25k_modified_g_1024
 from brinkhoff.b25k_modified_raw,dimensions
